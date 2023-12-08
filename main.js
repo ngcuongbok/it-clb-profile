93c3d9506b986954ba40290b2e1efdfddb4570d0
ngcuongbok <82893868+ngcuongbok@users.noreply.github.com>
2023-12-08 17:49:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,5 @@ getCloudflareJSON().then(data => {
   app.innerHTML += `div id="ip">
   <p>ip: <span class="content">${data.ip}</span></p>
   <p>Quốc gia: <span class="content">${data.loc} <img src="https://hatscripts.github.io/circle-flags/flags/${data.loc.toLowerCase()}.svg" width="40" height="40"/></span></p>
-  <p>ip: <span class="content"></span></p>
-  <p>ip: <span class="content"></span></p>
   </div>`
 })
